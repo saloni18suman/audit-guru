7b9949589a34978e028e0d1cd6d7288e69dd73eb
saloismanofficial <salonisumanofficial@gmail.com>
2026-06-29 20:21:08 +0530

    Fix langchain text splitter import for newer langchain versions

--- rag/policy_rag.py
+++ rag/policy_rag.py
@@ -8,7 +8,7 @@ import tempfile
 import boto3
 from botocore.exceptions import ClientError
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 
